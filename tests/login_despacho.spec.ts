5eef1881371f4eb149b0005c36d59f49ba46020d
mdorantese2e <dorantesm82@gmail.com>
2025-04-24 23:35:09 -0600

    Inicio e2e Rider

---
+++ tests/login_despacho.spec.ts
@@ -0,0 +1,24 @@
+import { test, expect } from '@playwright/test';
+
+import { LoginPage } from '../pageobjects/login/LoginPage';
+
+test('despacho', async ({ page }) => {
+
+    //await page.goto('https://www.riderlitigationfinance.com/')
+    await page.goto('https://rider-litigation-finance-web-stating-dot-total-pad-308418.uc.r.appspot.com/')
+   
+    await page.waitForTimeout(6_000)
+
+    await page.locator('//button[@class="btn btn-outline-light btn-sm"]').click()
+
+    await page.waitForTimeout(3_000);
+
+    const loginPage = new LoginPage(page);
+
+    await loginPage.doLogin('dorantesm82+e2edespacho2@gmail.com', 'BLQ4WDLJ');
+ 
+    await page.waitForTimeout(6_000);
+    //await page.pause();
+    await page.close();
+
+});
